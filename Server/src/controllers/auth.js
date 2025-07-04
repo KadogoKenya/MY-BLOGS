c7e8923b2b3fe3fdf70a9fb9513de28a809ee9c4
KadogoKenya <catherinekanini001@gmail.com>
2025-07-04 10:16:22 +0300

    created the auth file for to connect express

--- Server/src/controllers/auth.js
+++ Server/src/controllers/auth.js
@@ -1,38 +1,9 @@
-import { PrismaClient } from 'prisma/client';
-import bcrypt from 'bcrypt'; //for hasing the password-makes it appear in hash format
-
-const prisma = new PrismaClient();
-
-const signup = async (req, res) => {
-    try {
-        const { fullName, email, password } = req.body;
-        const existingUser = await prisma.user.findUnique({
-            where: {email}
-        });
-
-        if(existingUser){
-            return res.status(400).json({message: "user already existing"})
-        }
-        const harshedPassword = await bcrypt.hash(password,10)
-        const user = prisma.user.create({
-            data: {
-                fullName,
-                email,
-                password:harshedPassword,
-            },
-        });
-        return req.status(201).json({message: "user is created successfully",
-            user:{
-                id:user.id,
-                email:user.email
-            },
-    });
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({message:"internal server error"})
-    }
-    };
+import express from 'express'
+import { signup } from '../controllers/auth.controllers.js'
 
 
-export default signup
+ const router = express.Router();
+
+ router.post("/signup", signup)
+
+ export default router;
